2df57f17a0d16d86c263023f7350c1c9a144a386
atardecer01 <rivas.lina@correounivalle.edu.co>
2023-05-27 16:44:01 -0600

    conexiones

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 conectarDB();
 
-const dominiosPermitidos = ['http://localhost:3000'];
+const dominiosPermitidos = ['https://proyecto-seven-xi.vercel.app/'];
 
 const corsOptions = {
     origin: function(origin, callback) {
